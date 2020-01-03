4b24782b45c8fbf8bbae68a632614982fb02b276
vgutta <vineethg@udel.edu>
2020-01-02 23:10:58 -0500

    day 4 part 2

--- 2019/Day 4/day4.py
+++ 2019/Day 4/day4.py
@@ -29,4 +29,30 @@ for num in range(low, high):
         count += 1
 
 print(count)
-    
+
+
+######### Part 2 ################
+
+def adjacent_digits_part2(n):
+    str_int = str(n)
+    prev = None
+    consecutive = 1
+    for digit in str_int:
+        if digit == prev:
+            consecutive += 1
+        if digit != prev and consecutive == 2:
+            return True
+        if digit != prev:
+            consecutive = 1
+            prev = digit
+    if consecutive == 2:
+        return True
+    return False
+
+
+count2 = 0
+for num in range(low, high):
+    if adjacent_digits_part2(num) and increasing_digits(num):
+        count2 += 1
+
+print(count2)
